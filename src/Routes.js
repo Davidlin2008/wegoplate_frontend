1f459375f4378dba5a495a206f958fadae9a8452
jppark <pjp9157@gmail.com>
2020-01-07 19:35:18 +0900

    sign in commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main";
+import SignIn from "./Pages/SignIn";
 
 class Routes extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route path="/signin" component={SignIn} />
         </Switch>
       </Router>
     );
