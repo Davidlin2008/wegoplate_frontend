c9dd4e1fa849e57871ed83eb9e927e11db5d62d3
dahyeyudev <dahye.yu.dev@gmail.com>
2020-01-14 18:23:13 +0900

    api 확인 완료

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slide from "../Components/Main/Slide";
 import List from "../Components/Main/List";
-import { slide, list, highrate, eatdealing } from "../Components/Main/data";
+//import { slide, list, highrate, eatdealing } from "../Components/Main/data";
 
 export default function Main() {
-  // , { useEffect, useState }
-  // const [slide, setSlide] = useState([]);
-  // const [list, setList] = useState([]);
-  // const []
+  const [slide, setSlide] = useState([]);
+  const [list, setList] = useState([]);
+  const [eatdealing, setEatdealing] = useState([]);
+  const [highrate, setHighrate] = useState([]);
 
-  // const fetchSlide = async () => {
-  //   const res = await fetch("http://10.58.7.97:8000/restaurant/topic/1");
-  //   const data = await res.json();
-  //   setSlide(data.top_list);
-  // };
-  // const fetchList = async () => {
-  //   const res = await fetch("http://10.58.7.97:8000/restaurant/topic/4");
-  //   const data = await res.json();
-  //   setList(data.top_list);
-  // };
-  // const fetchList = async () => {
-  //   const res = await fetch("");
-  //   const data = await res.json();
-  //   setList(data.result);
-  // };
+  const fetchSlide = async () => {
+    const res = await fetch("http://10.58.7.97:8000/restaurant/topic/1");
+    const data = await res.json();
+    setSlide(data.top_list);
+  };
+  const fetchList = async () => {
+    const res = await fetch("http://10.58.7.97:8000/restaurant/topic/4");
+    const data = await res.json();
+    setList(data.top_list);
+  };
+  const fetchEatdealing = async () => {
+    const res = await fetch("http://10.58.1.195:8000/restaurant/2");
+    const data = await res.json();
+    setEatdealing(data.restaurant_list);
+  };
+  const fetchHighrate = async () => {
+    const res = await fetch("http://10.58.1.195:8000/restaurant/3");
+    const data = await res.json();
+    setHighrate(data.restaurant_list);
+  };
 
-  // useEffect(() => {
-  //   fetchSlide();
-  //   fetchList();
-  // }, []);
+  useEffect(() => {
+    fetchSlide();
+    fetchList();
+    fetchEatdealing();
+    fetchHighrate();
+  }, []);
+
   //훅스쓰면서 호출 보기
   //탭관리
   return (
