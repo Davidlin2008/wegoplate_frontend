22804ac2cd169841537e2e248c30bed69eef54fe
dahyeyudev <dahye.yu.dev@gmail.com>
2020-01-17 14:42:55 +0900

    컨플릭트해결 후 수정

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Index from "../../Components/Nav";
 import Header from "../../Components/Header";
 import "../../Styles/Common.scss";
+import Footer from "../../Components/Footer";
 
 export default class Main extends Component {
   render() {
